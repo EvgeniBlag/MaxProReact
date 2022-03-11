4749f4ef1f9f5003549471ae36bfb1fa21da60dc
EvgeniBlag <blagodarni@gmail.com>
2022-03-11 10:42:34 +0100

    img

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div >
+          <div className="logo">
+              <img src="https://cdn.svgporn.com/logos/lightstep.svg" alt=""/>
+          </div>  
+        </div>
+    )
+}
+export default Header;
