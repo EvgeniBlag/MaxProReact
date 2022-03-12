15b382dce130dd257acffe3569287ba901e4d3e8
EvgeniBlag <blagodarni@gmail.com>
2022-03-12 11:22:44 +0100

    valueInput

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./Articles.module.scss";
 
 const Articles = () =>{
+    const[city,setCity]=useState("Moscow");
+
     return (
         <div>
        <h1>Articles</h1>
-       <input type="text" value="wef" placeholder="Enter city" />
+
+       <input
+         type="text"
+         value={city}
+         placeholder="Enter city"
+         onChange={event=>setCity(event.target.value)}
+            />
+            
       <div className={styles.card}>
         <img src="" alt=""/>
         <h4></h4>
